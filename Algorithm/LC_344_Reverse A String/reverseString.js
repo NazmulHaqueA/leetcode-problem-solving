743218c4d40235b20ddf35aa1ae72ac961187b9d
NazmulHaqueA <naz089fvrict@gmail.com>
2022-04-20 15:39:59 +0600

    leetcode 344 - reverse string problem solved

---
+++ Algorithm/LC_344_Reverse A String/reverseString.js
@@ -0,0 +1,19 @@
+var reverseString = function(s) {
+   // return s.reverse();
+   let i = 0, j = s.length - 1;
+	while (i < j) {
+      [s[i], s[j]] = [s[j],s[i]];
+      i++;
+      j--;
+   }
+   return s;
+};
+
+console.log(reverseString(["h","e","l","l","o"]));   
+
+
+
+
+
+
+
